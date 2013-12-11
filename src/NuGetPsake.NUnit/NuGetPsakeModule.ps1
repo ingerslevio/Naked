db5b8992feb12c736b35be43a282d7c910dedf34
Emil Ingerslev <emil.ingerslev@gmail.com>
2013-12-11 20:50:04 +0100

    Updated nunit package according to new properties

--- src/NuGetPsake.NUnit/NuGetPsakeModule.ps1
+++ src/NuGetPsake.NUnit/NuGetPsakeModule.ps1
@@ -1,27 +1,27 @@
-task TestNUnit {
+task 'Test:NUnit' {
 
   if(-not $buildConfiguration.NUnitFiles) {
     throw "Could not find list of nunit files in property \$NUnitFiles"
   }
 
-  foreach($nunitFile in $buildConfiguration.NUnitFiles) {
+  foreach($nunitFile in $nugetPsake.buildConfiguration.NUnitFiles) {
     $testFile = $nunitFile
-    $nunit = (Get-ChildItem (join-path $packagesPath NUnit.Runners*\tools\nunit-console-x86.exe) | Sort LastWriteTime -Descending | Select-Object -First 1).FullName
+    $nunit = (Get-ChildItem (join-path $nugetPsake.properties.packagesPath NUnit.Runners*\tools\nunit-console-x86.exe) | Sort LastWriteTime -Descending | Select-Object -First 1).FullName
 
     $fullTestFileName = $testFile
     if(-not $fullTestFileName.endswith('.dll') -and -not $fullTestFileName.endswith('.exe')){
       $fullTestFileName = $testFile + '.dll'
     }
-    $testFilePath = join-path $solutionDir (Get-ChildItem $solutionDir -Name $fullTestFileName -Recurse)[0]
+    $testFilePath = join-path $nugetPsake.properties.solutionDir (Get-ChildItem $nugetPsake.properties.solutionDir -Name $fullTestFileName -Recurse)[0]
 
-    if($isRunningOnBuildServer){
-      & $teamcity['teamcity.dotnet.nunitlauncher'] v4.0 x86 NUnit-2.6.2 $testFilePath
+    if($nugetPsake.properties.isRunningOnBuildServer){
+      & $nugetPsake.properties.teamcity['teamcity.dotnet.nunitlauncher'] v4.0 x86 NUnit-2.6.2 $testFilePath
     } else {
       & $nunit $testFilePath /framework=v4.0 
     }
-    $script:hasFailedTests = $lastexitcode -ne 0
+    $nugetPsake.properties.hasFailedTests = $lastexitcode -ne 0
 
   }
 }
 
-Add-Dependency 'Test' 'TestNUnit'
+Add-Dependency 'Test' 'Test:NUnit'
